458f8f29918b09d24029ae710294b223e9080e30
Shivam kumar <shivam.kumar@usemultiplier.com>
2025-03-29 17:34:41 +0530

    Updated validation

---
+++ src/main/java/com/digipay/validators/UserValidator.java
@@ -0,0 +1,17 @@
+package com.digipay.validators;
+
+import com.digipay.rest.DigipayException;
+import com.digipay.rest.dtos.UserDto;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserValidator {
+
+    public void validateUserDetails(UserDto userDto) {
+        if (userDto == null) throw new DigipayException("Request Body can't be null", HttpStatus.BAD_REQUEST);
+        if (StringUtils.isEmpty(userDto.getName())) throw new DigipayException("Name can't be null or empty", HttpStatus.BAD_REQUEST);
+        if (userDto.getBalance() <= 0) throw new DigipayException("Balance can't be less than 0", HttpStatus.BAD_REQUEST);
+    }
+}
